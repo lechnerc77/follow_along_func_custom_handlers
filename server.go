d51cf07d8fe3874a2fb927395cddb19f52e2c4fd
Christian Lechner <drchristian.lechner@outlook.de>
2020-12-27 12:08:17 +0100

    added queue trigger

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -19,6 +20,39 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, message)
 }
 
+type InvokeRequest struct {
+	Data     map[string]json.RawMessage
+	Metadata map[string]interface{}
+}
+
+type InvokeResponse struct {
+	Outputs     map[string]interface{}
+	Logs        []string
+	ReturnValue interface{}
+}
+
+func queueTriggerHandler(w http.ResponseWriter, r *http.Request) {
+
+	var invokeRequest InvokeRequest
+
+	d := json.NewDecoder(r.Body)
+	d.Decode(&invokeRequest)
+
+	item := invokeRequest.Data["myQueueItem"]
+
+	outputs := map[string]interface{}{"": ""}
+
+	invokeResponse := InvokeResponse{outputs, nil, ""}
+
+	invokeResponse.Logs = append(invokeResponse.Logs, "hello from queue trigger")
+	invokeResponse.Logs = append(invokeResponse.Logs, string(item))
+
+	responseJson, _ := json.Marshal(invokeResponse)
+	fmt.Println(string(responseJson))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseJson)
+}
+
 func main() {
 	listenAddr := ":8080"
 
@@ -27,6 +61,7 @@ func main() {
 	}
 
 	http.HandleFunc("/api/hello", helloHandler)
+	http.HandleFunc("/queueTrigger", queueTriggerHandler)
 	log.Printf("About to listen on %s. Go to http://127.0.0.1%s", listenAddr, listenAddr)
 	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
